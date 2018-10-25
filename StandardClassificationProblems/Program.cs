b95e6616c0bf2c0692cdcd0723907e60a739ffcb
haochang <haochang@gpmcorp.com.tw>
2018-10-25 16:08:38 +0800

    first

---
+++ StandardClassificationProblems/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.Controls;
+using Accord.IO;
+using Accord.Math;
+using Accord.Statistics.Distributions.Univariate;
+using Accord.MachineLearning.Bayes;
+using System.Data;
+
+namespace StandardClassificationProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read the Excel worksheet into a DataTable
+            //!!Add reference Accord.IO to use ExcelReader
+            //!!Add System.Data to use DataTable
+            DataTable table = new ExcelReader("examples.xls").GetWorksheet("Classification - Yin Yang");
+
+            // Convert the DataTable to input and output vectors
+            double[][] inputs = table.ToJagged<double>("X", "Y");
+            int[] outputs = table.Columns["G"].ToArray<int>();
+
+            // Plot the data
+            //!!加入參考System.Windows.Forms
+            ScatterplotBox.Show("Yin-Yang", inputs, outputs).Hold();
+
+        }
+    }
+}
